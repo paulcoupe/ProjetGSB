0ee494161269dfc8c4dd01cef3173bba0b75ccd2
Pierre-YvesMinne <pierreyves.minne@gmail.com>
2016-11-25 23:51:14 +0100

    Ajout des commentaires dans le fichier 'index.php' Remise en forme du code

--- index.php
+++ index.php
@@ -1,31 +1,37 @@
-﻿
 <?php
+
 require_once("include/fct.inc.php");
 require_once ("include/class.pdogsb.inc.php");
+
+//On démarre la session
 session_start();
+
 $pdo = PdoGsb::getPdoGsb();
 $estConnecte = estConnecte();
+
+//On test si la variable $_REQUEST['uc'] est initialisé ou si l'utilisateur est déjà connecté
 if(!isset($_REQUEST['uc']) || !$estConnecte){
      $_REQUEST['uc'] = 'connexion';
-}	 
+}
+
 $uc = $_REQUEST['uc'];
-switch($uc){
+
+//En fonction de la variable $uc, on exécute l'un des cas suivants
+switch($uc){  
+        
+        //Si la variable est égale à 'connexion', on appelle le contrôleur 'c_connexion.php'
 	case 'connexion':{
 		include("controleurs/c_connexion.php");break;
-	}
+            }           
+        //Si la variable est égale à 'gererFrais', on appelle le contrôleur 'c_gererFrais.php'
 	case 'gererFrais' :{
 		include("controleurs/c_gererFrais.php");break;
-	}
+            }   
+        //Si la variable est égale à 'etatFrais', on appelle le contrôleur 'c_etatFrais.php'
 	case 'etatFrais' :{
 		include("controleurs/c_etatFrais.php");break; 
+            }
 	}
-	
-	
-	}
-	
-	
-
-
 ?>
 
 
