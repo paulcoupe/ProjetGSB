89c27f14b86542860b2dc837d887db2fabb41064
Pierre-YvesMinne <pierreyves.minne@gmail.com>
2016-11-26 00:25:11 +0100

    Ajout des commentaires dans le fichier 'controleurs/c_etatFrais.php' Remise en forme du code

--- controleurs/c_etatFrais.php
+++ controleurs/c_etatFrais.php
@@ -1,24 +1,31 @@
 ﻿<?php
-
 include("vues/v_sommaire.php");
+
 $action = $_REQUEST['action'];
 $idVisiteur = $_SESSION['idVisiteur'];
+
+//En fonction de la variable $action, on effectue l'un des cas suivants
 switch($action){
+        
+        //Si la variable est égale à 'selectionnerMois', on appelle la vue 'v_listeMois.php'
 	case 'selectionnerMois':{
+            	include("vues/v_listeMois.php");
 		$lesMois=$pdo->getLesMoisDisponibles($idVisiteur);
 		// Afin de sélectionner par défaut le dernier mois dans la zone de liste
 		// on demande toutes les clés, et on prend la première,
 		// les mois étant triés décroissants
 		$lesCles = array_keys( $lesMois );
 		$moisASelectionner = $lesCles[0];
-		include("vues/v_listeMois.php");
 		break;
 	}
+        
+        //Si la variable est égale à 'voirEtatFrais', on appelle les vues 'v_listeMois.php' et 'v_etatFrais.php'
 	case 'voirEtatFrais':{
+            	include("vues/v_listeMois.php");
+                include("vues/v_etatFrais.php");
 		$leMois = $_REQUEST['lstMois']; 
 		$lesMois=$pdo->getLesMoisDisponibles($idVisiteur);
 		$moisASelectionner = $leMois;
-		include("vues/v_listeMois.php");
 		$lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur,$leMois);
 		$lesFraisForfait= $pdo->getLesFraisForfait($idVisiteur,$leMois);
 		$lesInfosFicheFrais = $pdo->getLesInfosFicheFrais($idVisiteur,$leMois);
@@ -29,7 +36,6 @@ switch($action){
 		$nbJustificatifs = $lesInfosFicheFrais['nbJustificatifs'];
 		$dateModif =  $lesInfosFicheFrais['dateModif'];
 		$dateModif =  dateAnglaisVersFrancais($dateModif);
-		include("vues/v_etatFrais.php");
 	}
 }
 ?>
