91b9bdd91abe40c827af35839c99eacea68fb5b4
Pierre-YvesMinne <pierreyves.minne@gmail.com>
2016-11-26 01:50:03 +0100

    Remise en forme du code du fichier 'vues/listeFraisHorsForfait' et correction

--- vues/v_listeFraisHorsForfait.php
+++ vues/v_listeFraisHorsForfait.php
@@ -1,40 +1,50 @@
 ﻿<div class="col-md-6">
-	<div class="content-box-large">
-		<div class="panel-heading">
-			<legend>Elément hors forfait</legend>
-		</div>
-		<div class="panel-body">
-			<table class="table">
-			  <thead>
-				<tr>
-					<th class="date">Date</th>
-					<th class="libelle">Libellé</th>  
-					<th class="montant">Montant</th>  
-					<th class="action">&nbsp;</th>              
-				 </tr>
-				 <?php    
-
-					foreach( $lesFraisHorsForfait as $unFraisHorsForfait) 
-					{
-						$libelle = $unFraisHorsForfait['libelle'];
-						$date = $unFraisHorsForfait['date'];
-						$montant=$unFraisHorsForfait['montant'];
-						$id = $unFraisHorsForfait['id']
-						
-				?>		
-						<tr>
-							<td> <?php echo $date ?></td>
-							<td><?php echo $libelle ?></td>
-							<td><?php echo $montant ?></td>
-							<td> <?php if ($lesInfosFicheFrais['idEtat']!='CR') { echo '<a></a>';}
-								else {
-									echo '<a href="index.php?uc=gererFrais&action=supprimerFrais&idFrais='.$id.'"
-								onclick="return confirm(\'Voulez-vous vraiment supprimer ce frais?\');">Supprimer ce frais</a></td>';
-								}
-						?></tr><?php		 }  ?>	  
-			 </table>
-		</div> 
+    <div class="content-box-large">
+        
+	<div class="panel-heading">
+            <legend>Elément hors forfait</legend>          
 	</div>
+        
+	<div class="panel-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th class="date">Date</th>
+			<th class="libelle">Libellé</th>  
+			<th class="montant">Montant</th>  
+			<th class="action">&nbsp;</th>              
+                    </tr>
+                    
+                    <?php    
+			foreach( $lesFraisHorsForfait as $unFraisHorsForfait) 
+			{
+                            $libelle = $unFraisHorsForfait['libelle'];
+                            $date = $unFraisHorsForfait['date'];
+                            $montant=$unFraisHorsForfait['montant'];
+                            $id = $unFraisHorsForfait['id']			
+                    ?>	
+                    
+                    <tr>
+			<td> <?php echo $date ?></td>
+			<td><?php echo $libelle ?></td>
+			<td><?php echo $montant ?></td>
+			<td>
+                            <?php 
+                                if ($lesInfosFicheFrais['idEtat']!='CR') { 
+                                        echo '<a></a>';
+                                }
+				else {
+                                    echo '<a href="index.php?uc=gererFrais&action=supprimerFrais&idFrais='.$id.'"
+					onclick="return confirm(\'Voulez-vous vraiment supprimer ce frais?\');">Supprimer ce frais</a></td>';
+				}
+						?>
+                    </tr> 
+                        <?php		 
+                                }  
+                        ?>	  
+            </table>
+	</div> 
+    </div>
 </div>
 
 
