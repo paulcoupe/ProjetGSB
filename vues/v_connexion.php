78b7a91fcf7321555e24fdc9c63b973ebf9d42bb
Pierre-YvesMinne <pierreyves.minne@gmail.com>
2016-11-26 01:07:03 +0100

    Remise en forme du code du fichier 'vues/v_connexion' et correction

--- vues/v_connexion.php
+++ vues/v_connexion.php
@@ -14,40 +14,34 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
-  </head>
+</head>
 
-  <body background="assets/img/laboratoire.jpg">
+<body background="assets/img/laboratoire.jpg">
 
-      <!-- **********************************************************************************************************************************************************
-      MAIN CONTENT
-      *********************************************************************************************************************************************************** -->
-
-<div class="page-content container">
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4">
-			<div class="login-wrapper">
-				<div class="box">
-					<div class="content-wrap">
-						<legend>Se connecter</legend>
-							<form method="post" action="index.php?uc=connexion&action=valideConnexion">
-								<input name="login" class="form-control" type="text" placeholder="Login">
-								<input name="mdp" class="form-control" type="password" placeholder="Password">
-								</br>
-								<input type="submit" class="btn btn-primary signup" value="Login">
-							</form>
-							</br>
-						
-					</div>							
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+    <div class="page-content container">
+            <div class="row">
+                    <div class="col-md-4 col-md-offset-4">
+                            <div class="login-wrapper">
+                                    <div class="box">
+                                            <div class="content-wrap">
+                                                    <legend>Se connecter</legend>
+                                                            <form method="post" action="index.php?uc=connexion&action=valideConnexion">
+                                                                    <input name="login" class="form-control" type="text" placeholder="Login">
+                                                                    <input name="mdp" class="form-control" type="password" placeholder="Password">
+                                                                    </br>
+                                                                    <input type="submit" class="btn btn-primary signup" value="Login">
+                                                            </form>
+                                            </div>							
+                                    </div>
+                            </div>
+                    </div>
+            </div>
+    </div>
 
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="js/custom.js"></script>
-  </body>
+</body>
 </html>
